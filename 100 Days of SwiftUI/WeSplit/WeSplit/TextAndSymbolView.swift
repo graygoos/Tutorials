12e34d709fcb58470cdf6a91d1a1a323dd6b3d07
graygoos <femialiu713@gmail.com>
2023-07-12 12:11:51 +0100

    created 100 Days of SwiftUI folder and moved WeSplit project into it

---
+++ 100 Days of SwiftUI/WeSplit/WeSplit/TextAndSymbolView.swift
@@ -0,0 +1,26 @@
+//
+//  TextAndSymbolView.swift
+//  WeSplit
+//
+//  Created by Femi Aliu on 12/07/2023.
+//
+
+import SwiftUI
+
+struct TextAndSymbolView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "star")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Aloha, world!")
+                .foregroundColor(.brown)
+        }
+    }
+}
+
+struct TextAndSymbolView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextAndSymbolView()
+    }
+}
