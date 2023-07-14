28acdd63f7a505fb43588cf13744f4eb95af3f3f
graygoos <femialiu713@gmail.com>
2023-07-14 12:46:18 +0100

    Created Project 2 project file

---
+++ 100 Days of SwiftUI/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Femi Aliu on 14/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
